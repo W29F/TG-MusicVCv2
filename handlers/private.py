ec6b441eba55debd491d98f4b958a239c045cd08
nerawgg@gmail.com <nerawgg@gmail.com>
2021-04-22 15:53:49 +0700

    TG-MusicVCv2

---
+++ handlers/private.py
@@ -0,0 +1,46 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+from helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+    await message.reply_sticker("CAACAgQAAx0CTv65QgABBfJlYF6VCrGMm6OJ23AxHmD6qUSWESsAAhoQAAKm8XEeD5nrjz5IJFYeBA")
+    await message.reply_text(
+        f"""**Hey, I'm {bn} 🎵
+
+I can play music in your group's voice call. Developed by [Owner](https://t.me/privatener).
+
+Add me to your group and play music freely!**
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💬 Channel", url="https://t.me/lovemelikeuloveurfather"
+                    )
+                ],[ 
+                    InlineKeyboardButton(
+                        "🔊 Support ➕", url="https://t.me/lovemelikeuloveurfather"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""**Group Music Player Online ✅**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔊 Support ", url="https://t.me/lovemelikeuloveurfather")
+                ]
+            ]
+        )
+   )
+
+
